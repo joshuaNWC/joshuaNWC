e2dceb9cafaa2e3f8a52f758ed22937b3a6fb2eb
joshuanwc <152532365+joshuanwc@users.noreply.github.com>
2023-12-11 11:08:16 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //get all elements from collapsible class
 let coll = document.getElementsByClassName("MenuButtons");
 //i variable for loop
-let i;
 //forloop through each element in collapsible
-for (i = 0; i < coll.length; i++) {
+for (let i = 0; i < coll.length; i++) {
   //click event for Menu Buttons
   coll[i].addEventListener("click", function () {
     //active toggle
