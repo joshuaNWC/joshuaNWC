0d8313c4cd8b69fc8252aa11cf82282c88cf1fef
ShojiMoraga <147573394+ShojiMoraga@users.noreply.github.com>
2023-12-11 11:17:53 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //get all elements from collapsible class
 let coll = document.getElementsByClassName("MenuButtons");
 //i variable for loop
+let i;
 //forloop through each element in collapsible
-for (let i = 0; i < coll.length; i++) {
+for (i = 0; i < coll.length; i++) {
   //click event for Menu Buttons
   coll[i].addEventListener("click", function () {
-    //active toggle
-    this.classList.toggle("active");
-    //sets max height to content
+    //Gets the content in the Html
     let content = this.nextElementSibling;
+    //Sets Height to Content Height
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
