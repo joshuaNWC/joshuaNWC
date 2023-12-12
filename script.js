5fb4bf863b8a5ab4053db3a4ba516d62ff3ca20a
joshuanwc <152532365+joshuanwc@users.noreply.github.com>
2023-12-12 11:07:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+let futureDay = new Date("Sep 1, 2024 1:0:1").getTime();
+let x = setInterval(function () {
+  let now = new Date().getTime();
+
+  let distance = futureDay - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="timer"
+  document.getElementById("timer").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+}, 1000);
